37a6a0ffc6617a92e0dc13f7a83682b318dc08c5
AnryZZ <anryzz.github.io>
2020-06-05 14:37:01 +0400

    3й шаг

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from '../modules/readline-sync/lib/readline-sync.js';
+import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
   const name = readlineSync.question('Your answer: ');
 export default name;
