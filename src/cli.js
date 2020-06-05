6e9e69a2ae07ac439eaf65321710ba6b07abbe81
AnryZZ <anryzz.github.io>
2020-06-05 15:03:39 +0400

    3й шаг

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
+  console.log('Welcom to the Brain Games!');
   const name = readlineSync.question('Your answer: ');
+  console.log('Hello, ' + name + '!');
 export default name;
