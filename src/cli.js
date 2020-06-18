91bb4ecc238ca9d85719add3dc8f923b22325058
AnryZZ <anryzz.github.io>
2020-06-18 15:51:59 +0400

    шаг 6

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,17 @@
 import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
 
-export const str0 = console.log('Welcom to the Brain Games!');
+// Приветствие и получение имени.
+export const str0 = console.log('Welcome to the Brain Games!');
 export const name = readlineSync.question('May I have your name? ');
-const str1 = 'Hello, ';
-const str2 = '!';
-export const str3 = console.log(str1 + name + str2);
+export const str1 = console.log(`Hello, ${name}!`);
+// Механизм построения строки правильного/непрвильного ответа.
+export const answer = ['memory for answer'];
+export const strBuilder = (anyAnswer) => {
+  const strWrong = `'${anyAnswer}' is wrong answer ;(. Correct answer was '${answer[0]}'. Let's try again, ${name}!`;
+  const strRight = 'Correct!';
+  if (anyAnswer === answer[0]) {
+    return strRight;
+  }
+  return strWrong;
+};
+export const strEndGame = `Congratulations, ${name}!`;
--- src/even.js
+++ src/even.js
@@ -1,36 +1,25 @@
 import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
-import { name } from './cli.js';
+import { answer, strBuilder, strEndGame } from './cli.js';
 
-let endgame = 0;
-const congratulations = 'Congratulations, ';
-const symbol = '!';
-const lastString = congratulations + name + symbol;
+let endgame = 1;
 for (let i = 0; i < 3; i += 1) {
   const number = Math.ceil(Math.random() * 100);
   const num = number % 2;
   console.log('Is this an even number? Answer `yes` or `no`.');
   console.log(number);
   const otvet = readlineSync.question('Enter a response: ');
-  if (num === 0 && otvet === 'yes') {
-    console.log('Correct!');
-    endgame = 1;
-  } else if (num === 0 && otvet === 'no') {
-    console.log('`no` is wrong answer ;(. Correct answer was `yes`');
-    endgame = 0;
-    break;
-  } else if (num !== 0 && otvet === 'no') {
-    console.log('Correct!');
-    endgame = 1;
-  } else if (num !== 0 && otvet === 'yes') {
-    console.log('`yes` is wrong answer ;(. Correct answer was `no`');
-    endgame = 0;
-    break;
+  if (num === 0) {
+    answer[0] = 'yes';
+    console.log(strBuilder(otvet));
   } else {
-    console.log('wrong answer ;(');
+    answer[0] = 'no';
+    console.log(strBuilder(otvet));
+  }
+  if (answer[0] !== otvet) {
     endgame = 0;
     break;
   }
 }
 if (endgame === 1) {
-  console.log(lastString);
+  console.log(strEndGame);
 }
