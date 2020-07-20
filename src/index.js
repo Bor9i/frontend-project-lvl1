ddb313c5015f1dc32a6fe45ed9df3ab445ffa679
AnryZZ <anryzz.github.io>
2020-07-20 18:35:26 +0400

    рефакторинг 5

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
 
 export const numberOfGames = 3;
 
-export const gameEngine = (anyQuestion, anyGameDataList) => {
+export const gameEngine = (anyTask, anyGameDataList) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   for (let i = 0; i < numberOfGames; i += 1) {
-    const [task, result] = anyGameDataList[i];
-    console.log(anyQuestion);
-    console.log(task);
+    const [gameQuestion, result] = anyGameDataList[i];
+    console.log(anyTask);
+    console.log(gameQuestion);
     const inputText = readlineSync.question('Your answer: ');
     if (result === inputText) {
       console.log('Correct!');
